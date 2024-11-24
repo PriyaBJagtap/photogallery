277d6bead2567ec77b184c67d246262817ca78b2
PriyaBJagtap <priyababajijagtap1996@gmail.com>
2024-11-24 18:39:27 +0530

    changes in ts file

---
+++ .angular/cache/17.3.8/vite/deps/@angular_animations.js
@@ -0,0 +1,50 @@
+import {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationGroupPlayer,
+  AnimationMetadataType,
+  BrowserAnimationBuilder,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  ɵPRE_STYLE
+} from "./chunk-VFR7CRRE.js";
+import "./chunk-ML24ZVSQ.js";
+import "./chunk-6JLT4FPK.js";
+import "./chunk-J4B6MK7R.js";
+export {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationMetadataType,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  AnimationGroupPlayer as ɵAnimationGroupPlayer,
+  BrowserAnimationBuilder as ɵBrowserAnimationBuilder,
+  ɵPRE_STYLE
+};
+//# sourceMappingURL=@angular_animations.js.map
